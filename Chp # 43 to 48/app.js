523d50c641f1924150d7e5e6fdb5f4f1303cde56
alishbanazar <alishbanazarmuhammadabbasi@gmail.com>
2024-08-04 10:26:35 +0500

    events done

---
+++ Chp # 43 to 48/app.js
@@ -0,0 +1,38 @@
+//TASK 1
+//1. Show an alert box on click on a link.
+
+// function greet(){
+//     alert("Hi user! hope you are doing well🥰")
+// }
+
+
+
+// TASK 2
+//Display some Mobile images in browser. On click on an image Show the message in alert to user.
+
+// function thanks() {
+//     alert("Thnaks for opurchasing a phone from us🤩")
+    
+// }
+
+// TASK 4
+//Display an image in browser. Change the picture on mouseover and set the first picture on mouseout.
+ 
+//solution in Html
+
+//TASK 5
+//Show a counter in browser. Counter should increase on click on increase button and decrease on click on decrease button. And show updated counter
+//value in browser.
+
+// var counter = 0;
+// function increase() {
+//     document.getElementById("counter").innerText = counter++
+    
+// }
+// function decrease() {
+//     if(counter<0){
+//         counter = 0;
+//     }
+//     document.getElementById("counter").innerText = counter--
+    
+// }
